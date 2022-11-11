83ed72b9de98a72c1c5b6d6abbeb581a4162a246
Kelsie Porter <krporter22@gmail.com>
2022-11-11 14:15:35 -0700

    Add #encrypted_message

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,12 +7,8 @@ class Enigma < Cypher
     the_keys = split_keys(key)
     the_offsets = offsets(date)
     the_shifts = shifts(the_keys, the_offsets)
-    encrypted_message = ''
-    message.chars.each do |char|
-      encrypted_message += shifted_alphabet[char]
-    end 
     {
-      encryption: encrypted_message,
+      encryption: encrypted_message(message, the_shifts),
       key: key,
       date: date
     }
@@ -24,5 +20,34 @@ class Enigma < Cypher
       key: key,
       date: date
     }
+  end
+
+  def encrypted_message(message, the_shifts)
+    counter = 0
+    output = ''
+    message.chars.each do |letter|
+      if counter == 0
+        shifted_alphabet = alphabet.rotate(the_shifts[:a])
+        index_position = alphabet.find_index(letter)
+        output += shifted_alphabet[index_position]
+        counter += 1
+      elsif counter == 1
+        shifted_alphabet = alphabet.rotate(the_shifts[:b])
+        index_position = alphabet.find_index(letter)
+        output += shifted_alphabet[index_position]
+        counter += 1
+      elsif counter == 2
+        shifted_alphabet = alphabet.rotate(the_shifts[:c])
+        index_position = alphabet.find_index(letter)
+        output += shifted_alphabet[index_position]
+        counter += 1
+      elsif counter == 3
+        shifted_alphabet = alphabet.rotate(the_shifts[:d])
+        index_position = alphabet.find_index(letter)
+        output += shifted_alphabet[index_position]
+        counter = 0 
+      end
+    end 
+    output
   end
 end
