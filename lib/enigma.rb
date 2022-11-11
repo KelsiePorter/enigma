62469da967b5255b227f997a3b6cee3e617a577b
Kelsie Porter <krporter22@gmail.com>
2022-11-10 20:49:23 -0700

    Create private #key_generator

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,7 @@
 class Enigma 
   CHARACTER_SET = ("a".."z").to_a << " "
 
-  def encrypt(message, key = "", date = today)
+  def encrypt(message, key = key_generator, date = today)
     {
       encryption: message,
       key: key,
@@ -13,5 +13,9 @@ class Enigma
 
   def today 
     Date.today.strftime("%m%d%y")
+  end
+
+  def key_generator 
+    (10000..99999).to_a.sample.to_s
   end
 end
