db356e21e425bebdaf218c4e4240f1aa4d070342
Kelsie Porter <krporter22@gmail.com>
2022-11-11 10:51:14 -0700

    Create #shifts and move to Cypher parentclass

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,13 @@
 require_relative 'generator'
+require_relative 'cypher'
 
-class Enigma 
+class Enigma < Cypher 
   include Generator
 
   def encrypt(message, key = random_key, date = today)
     the_keys = split_keys(key)
     the_offsets = offsets(date)
+    the_shifts = shifts(the_keys, the_offsets)
     {
       encryption: message,
       key: key,
@@ -20,30 +22,4 @@ class Enigma
       date: date
     }
   end
-
-  def split_keys(key)
-    {
-      a: key[0..1].to_i,
-      b: key[1..2].to_i,
-      c: key[2..3].to_i,
-      d: key[3..4].to_i
-    }
-  end
-
-  def offsets(date)
-    integer_date = date.to_i
-    squared_date = integer_date * integer_date
-    last_four_characters = squared_date.to_s[-4..-1]
-    last_four_digits_array = last_four_characters.chars
-    {
-      a_offset: last_four_digits_array[0].to_i,
-      b_offset: last_four_digits_array[1].to_i,
-      c_offset: last_four_digits_array[2].to_i,
-      d_offset: last_four_digits_array[3].to_i
-    }
-  end
-
-  def shifts
-  end
-
 end
