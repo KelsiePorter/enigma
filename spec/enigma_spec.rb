c6bd3786cad5b239d5be622d66e34983770ef541
Kelsie Porter <krporter22@gmail.com>
2022-11-10 19:50:10 -0700

    Create test structure for #encrypt

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -7,4 +7,33 @@ RSpec.describe Enigma do
 
     expect(enigma).to be_an(Enigma)
   end
+  
+  describe '#encrypt' do 
+    it 'receives a message and encrypts it' do 
+      enigma = Enigma.new
+      expected = enigma.encrypt("Hello World", "02715", "040895")
+
+      expect(expected).to be_an(Hash)
+      expect(expected.keys.size).to eq(3)
+      expect(expected.keys).to eq([:encryption, :key, :date])
+    end
+
+    xit 'the user can define the encryption key' do 
+      # add expectation for when the user tries to provide a date but not a key
+      # incorrect number of characters will help tell if the user is trying to pass a date
+      # and not a key
+    end
+
+    xit 'the user can define the encryption key and the date' do 
+    end
+
+  end
+
+  describe '#decrypt' do 
+    xit 'receives an encrypted message and decrypts it' do 
+      # expect that it returns a hash (xyz.class).to eq(Hash)
+      # expect that the hash has three keys xyz.keys.size.to eq(3)
+      # expect that the hash has three keys xyz.keys.to eq([:decryption, :key, :date])
+    end
+  end
 end
