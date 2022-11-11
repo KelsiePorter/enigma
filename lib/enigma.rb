52194b5d72a4ebcf68ee46594dc8127da841f061
Kelsie Porter <krporter22@gmail.com>
2022-11-10 19:50:51 -0700

    Create structure for #encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,11 @@
 class Enigma 
   CHARACTER_SET = ("a".."z").to_a << " "
+
+  def encrypt(message, key = "", date = "")
+    {
+      encryption: message.upcase,
+      key: key,
+      date: date
+    }
+  end
 end
