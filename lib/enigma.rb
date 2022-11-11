6d747e8251c2ca35a5c8594d116cc64d54e171cb
Kelsie Porter <krporter22@gmail.com>
2022-11-11 14:41:13 -0700

    Create #decrypted_message

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -15,8 +15,11 @@ class Enigma < Cypher
   end
 
   def decrypt(encrypted_message, key, date = today)
+    the_keys = split_keys(key)
+    the_offsets = offsets(date)
+    the_shifts = shifts(the_keys, the_offsets)
     {
-      decryption: encrypted_message,
+      decryption: decrypted_message(encrypted_message, the_shifts),
       key: key,
       date: date
     }
@@ -46,6 +49,35 @@ class Enigma < Cypher
         index_position = alphabet.find_index(letter)
         output += shifted_alphabet[index_position]
         counter = 0 
+      end
+    end 
+    output
+  end
+
+  def decrypted_message(message, the_shifts)
+    counter = 0
+    output = ''
+    message.chars.each do |letter|
+      if counter == 0
+        shifted_alphabet = alphabet.rotate(-the_shifts[:a])
+        index_position = alphabet.find_index(letter)
+        output += shifted_alphabet[index_position]
+        counter += 1
+      elsif counter == 1
+        shifted_alphabet = alphabet.rotate(-the_shifts[:b])
+        index_position = alphabet.find_index(letter)
+        output += shifted_alphabet[index_position]
+        counter += 1
+      elsif counter == 2
+        shifted_alphabet = alphabet.rotate(-the_shifts[:c])
+        index_position = alphabet.find_index(letter)
+        output += shifted_alphabet[index_position]
+        counter += 1
+      elsif counter == 3
+        shifted_alphabet = alphabet.rotate(-the_shifts[:d])
+        index_position = alphabet.find_index(letter)
+        output += shifted_alphabet[index_position]
+        counter = 0
       end
     end 
     output
