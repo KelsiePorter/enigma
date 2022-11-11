2350c13a1571680a693dbbbef6961cb229356ea9
Kelsie Porter <krporter22@gmail.com>
2022-11-11 08:41:06 -0700

    Create #split_keys

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,6 +4,7 @@ class Enigma
   include Generator
 
   def encrypt(message, key = random_key, date = today)
+    the_keys = split_keys(key)
     {
       encryption: message,
       key: key,
@@ -18,4 +19,20 @@ class Enigma
       date: date
     }
   end
+
+  def split_keys(key)
+    {
+      a: key[0..1].to_i,
+      b: key[1..2].to_i,
+      c: key[2..3].to_i,
+      d: key[3..4].to_i
+    }
+  end
+
+  def offsets
+  end
+
+  def shifts
+  end
+
 end
