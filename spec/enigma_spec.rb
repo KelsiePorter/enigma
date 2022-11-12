b6c58dde9a6a1170970049bae00e245b34cc1043
Kelsie Porter <krporter22@gmail.com>
2022-11-12 14:44:49 -0700

    Fix: #today stubs and housekeeping

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -11,62 +11,80 @@ RSpec.describe Enigma do
   
   describe '#encrypt' do 
     let(:enigma) { Enigma.new }
-    let(:today) { Date.today.strftime("%m%d%y") }
+    let(:today) { Date.today.strftime("%d%m%y") }
 
-    it 'receives a message and encrypts it' do
+    it 'can return the encryption hash' do 
+      expected = enigma.encrypt("Hello World", "02715", "040895")
+
+      expect(expected).to eq({
+                              :date=>"040895", 
+                              :encryption=>"keder ohulw", 
+                              :key=>"02715"
+                            })
+    end
+
+    it 'receives a message with unique characters and encrypts it' do
       expected = enigma.encrypt("Hello World", "02715", "040895")
       expected_2 = enigma.encrypt("HELLO WORLD", "02715", "040895")
       expected_3 = enigma.encrypt("!HeLLo& %WOrLD!", "02715", "040895") 
 
-      expect(expected).to be_an(Hash)
       expect(expected.keys.size).to eq(3)
       expect(expected.keys).to eq([:encryption, :key, :date])
       expect(expected[:encryption]).to eq("keder ohulw")
       expect(expected_2[:encryption]).to eq("keder ohulw")
       expect(expected_3[:encryption]).to eq("!keder& %ohulw!")
-      expect(expected[:key]).to eq("02715")
-      expect(expected[:date]).to eq("040895")
     end
 
     it 'the user can define the encryption key but not the date' do 
-      # allow(enigma).to receive(:date).and_return("111122")
+      allow(enigma).to receive(:today).and_return("121122")
       expected = enigma.encrypt("Hello World", "02715")
-      # Is this going to fail when I run this test tomorrow? Since the date will be diff?
-      # expect(expected[:encryption]).to eq("rmjdyhugatb")
+     
+      expect(expected[:encryption]).to eq("rmjdyhugatb")
       expect(expected[:key]).to eq("02715")
-      expect(expected[:date]).to eq(today)
+      expect(expected[:date]).to eq("121122")
+      expect(expected).to eq({
+                              :date=>"121122", 
+                              :encryption=>"rmjdyhugatb", 
+                              :key=>"02715"
+                            })
     end
 
     it 'the user does not have to define the encryption key or date' do 
+      allow(enigma).to receive(:today).and_return("121122")
+      allow(enigma).to receive(:random_key).and_return("40794")
       expected = enigma.encrypt("Hello World")
       key_array = expected[:key].chars.map { |num| num.to_i.to_s }
-      # how would I test the encrypted message on this one since it changes
-      # every time the test is run?
+
       expect(expected[:encryption].length).to eq(11)
       expect(expected[:key]).to be_an(String)
       expect(expected[:key].length).to eq(5)
       expect(key_array.join).to eq(expected[:key])
-      expect(expected[:date]).to eq(today)
-    end
-    
-    it 'can return the encryption hash' do 
-      expected = enigma.encrypt("Hello World", "02715", "040895")
-
-      expect(expected).to be_a(Hash)
+      expect(expected).to eq({
+                              :date=>"121122", 
+                              :encryption=>"btrbiobel j", 
+                              :key=>"40794"
+                            })
     end
   end
 
   describe '#decrypt' do 
     let(:enigma) { Enigma.new }
-    let(:today) { Date.today.strftime("%m%d%y") }
+    let(:today) { Date.today.strftime("%d%m%y") }
+
+    it 'can return the decryption hash' do 
+      expected = enigma.decrypt("keder ohulw", "02715", "040895")
+      
+      expect(expected).to eq({
+                              :date=>"040895", 
+                              :decryption=>"hello world", 
+                              :key=>"02715"
+                            })
+    end
 
     it 'receives an encrypted message and decrypts it' do
-      # why does this test and the below test not work the same way!? 
       expected = enigma.decrypt("Keder oHulW", "02715", "040895")
       expected_2 = enigma.decrypt("%Keder oHulW&", "02715", "040895")
       
-
-      expect(expected).to be_an(Hash)
       expect(expected.keys.size).to eq(3)
       expect(expected.keys).to eq([:decryption, :key, :date])
       expect(expected[:decryption]).to eq("hello world")
@@ -76,23 +94,18 @@ RSpec.describe Enigma do
     end
 
     it 'the user can define the decryption key but not the date' do 
+      allow(enigma).to receive(:today).and_return("121122")
       encrypted = enigma.encrypt("hello world", "02715")
       expected = enigma.decrypt(encrypted[:encryption], "02715") 
 
       expect(expected[:decryption]).to eq("hello world")
       expect(expected[:key]).to eq("02715")
-      expect(expected[:date]).to eq(today)
+      expect(expected[:date]).to eq("121122")
 
       encrypted_2 = enigma.encrypt("HELLO WORLD", "02715")
       expected_2 = enigma.decrypt(encrypted[:encryption], "02715") 
 
       expect(expected_2[:decryption]).to eq("hello world")
-    end
-
-    it 'can return the decryption hash' do 
-      expected = enigma.decrypt("keder ohulw", "02715", "040895")
-      
-      expect(expected).to be_a(Hash)
     end
   end
 end
