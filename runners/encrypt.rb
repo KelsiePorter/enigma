95ed6c7a5a766f701875e68a6179416c2b5c05ec
Kelsie Porter <krporter22@gmail.com>
2022-11-12 17:57:45 -0700

    Create new encrypt runner

--- runners/encrypt.rb
+++ runners/encrypt.rb
@@ -1,4 +1,16 @@
-require_relative './encryption_runner.rb'
+require './lib/enigma'
+require 'date'
 
-Runner.encrypt
-Runner.write_encryption
+enigma = Enigma.new
+
+file = File.open(ARGV[0], "r")
+message = file.read 
+file.close
+
+encrypted_message = enigma.encrypt(message.chomp)
+
+file = File.open(ARGV[1], "w")
+file.write(encrypted_message[:encryption])
+file.close
+
+puts "Created '#{ARGV[1]}' with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"  
