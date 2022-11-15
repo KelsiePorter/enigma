bc5e5756389b502da5a896b96f55898aadd3f2d9
Kelsie Porter <krporter22@gmail.com>
2022-11-14 17:47:16 -0700

    Move methods to runner.rb

--- runners/decrypt.rb
+++ runners/decrypt.rb
@@ -1,17 +1,3 @@
-require './lib/enigma'
-require 'date'
+require_relative 'runner'
 
-enigma = Enigma.new
-
-file = File.open(ARGV[0], "r")
-message = file.read 
-file.close
-
-# Why wont this decrypt properly!?
-decrypted_message = enigma.decrypt(message.chomp, ARGV[2], ARGV[3])
-# require 'pry'; binding.pry
-file = File.open(ARGV[1], "w")
-file.write(decrypted_message[:decryption])
-file.close
-
-puts "Created '#{ARGV[1]}' with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
+Runner.new.decrypt(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
